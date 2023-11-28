5165f3f2d0b52902af2163742a19c56a4e99b56e
eggoch <78937837+eggoch@users.noreply.github.com>
2023-11-28 18:36:32 -0500

    Background additions, React functionality
    Rollback possible

---
+++ my-app/src/Page.style.jsx
@@ -0,0 +1,32 @@
+// import styled from "styled-components";
+
+// export const Page = styled.div`
+// min-height: 100vh;
+// min-width: 100vw;
+// display: flex;
+// flex-direction: row;
+// align-items: center;
+// background-color: black;
+// `;
+
+// export const NanoHeader = styled.div`
+//   height:8vh;
+//   width:100vw;
+//   position: absolute;
+//   top: 0;
+//   background-color: #c08989;
+  
+// `
+// export const NanoLinkWrapper = styled.a`
+//     height: inherit;
+//     width: 1vw;
+//     top: inherit;
+//     background-color: red;
+// `
+
+// export const NanoLink = styled.div`
+//     height: inherit;
+//     width: 15vw;
+//     top: inherit;
+//     background-color: red;
+// `
