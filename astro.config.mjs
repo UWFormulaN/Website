d9e34641eeb5d913a9fc1e68f2db830b53711094
RyanM1235 <94492798+RyanM1235@users.noreply.github.com>
2024-05-03 02:00:24 -0400

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,4 @@ import icon from "astro-icon"
 // https://astro.build/config
 export default defineConfig({
     integrations: [icon()],
-    site: 'https://github.com/UWFormulaN/Website.github.io',
-    base: 'src'
 });
