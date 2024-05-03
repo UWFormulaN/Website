2bb7e0e3be347d074523fa9ef347b83449731119
RyanM1235 <94492798+RyanM1235@users.noreply.github.com>
2024-05-03 01:44:11 -0400

    fixes, maybe

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import icon from "astro-icon"
 // https://astro.build/config
 export default defineConfig({
     integrations: [icon()],
-    site: 'https://github.com/UWFormulaN/Website.github.io'
+    site: 'https://github.com/UWFormulaN/Website.github.io',
+    base: 'src'
 });
