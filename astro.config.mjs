af1d73467ff74de5e5da61ab19de6575e4759dfa
Matthew Berger <41124308+bergermaestro@users.noreply.github.com>
2024-10-24 00:20:02 -0400

    configure cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   integrations: [tailwind(), icon(), react()],
+  adapter: cloudflare({
+    imageService: 'cloudflare'
+  }),
 });
